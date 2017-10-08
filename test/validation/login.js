76dd87a391290d6c2fc76a70926162e34d94db1d
Rich McRary <rgmcrary@gmail.com>
2017-10-08 18:50:50 -0400

    Testing validation scripts

--- test/validation/login.js
+++
@@ -1,9 +0,0 @@
-var Joi = require('joi');
-
-module.exports = {
- body: {
-   username: Joi.string().regex(/[a-zA-Z0-9]{3,30}/).required(),
-   password: Joi.string().regex(/[a-zA-Z0-9]{3,30}/).required()
- }
-};
-
