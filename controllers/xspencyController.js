c6cf4538f56d34ac676667333f45c4106ab22061
Rich McRary <rgmcrary@gmail.com>
2017-10-08 14:07:29 -0400

    Updated comments

--- controllers/xspencyController.js
+++ controllers/xspencyController.js
@@ -2,13 +2,13 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
+// Import the model (xspency.js) to use its database functions.
 var xspency = require("../models/xspency.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
     res.render("login", {});
-//   project.all(function(data) {
+//   xspency.all(function(data) {
 //     var hbsObject = {
 //       cats: data
 //     };
