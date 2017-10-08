2f7e1f590904114b1dc0386f8399980fbf18c274
Rich McRary <rgmcrary@gmail.com>
2017-10-08 18:50:22 -0400

    testing scripts

--- test/test.js
+++ test/test.js
@@ -1,26 +1,118 @@
-"use strict";
+'use strict';
 
-var expect = require("chai").expect;
-var titleize = require("../Login");
+var chai = require('chai');
+//var expect = require("chai").expect;
+var chaiHttp = require('chai-http');
+var should = chai.should();
+var login = require('../controllers/xspencyController');
+var server = require('../server');
 
-describe("Login", function() {
-  it("to capitalize initial letter of each word in input", function() {
-    expect(login("seth godin")).to.equal("Seth Godin");
+chai.use(chaiHttp);
+
+describe('The Xspency controller', function() {
+
+  // Login route testing
+  describe('login route', function() {
+    it('should return a 400 response if there is missing password', function(
+      done
+    ) {
+      var login = {
+        username: 'rmcrary',
+        password: ''
+      };
+      chai
+        .request(server)
+        .post('/api/login')
+        .send(login)
+        .end(function(err, res) {
+          res.should.have.status(400);
+          done();
+        });
+    });
+
+    it('should return a 400 response if there is missing username', function(
+      done
+    ) {
+      var login = {
+        username: '',
+        password: 'Rlgh45'
+      };
+      chai
+        .request(server)
+        .post('/api/login')
+        .type('form')
+        .send(login)
+        .end(function(err, res) {
+          res.should.have.status(400);
+          done();
+        });
+    });
+
+    it('should have valid data input in username', function(done) {
+      var login = {
+        username: '*&$(',
+        password: 'Rlgh45'
+      };
+      chai
+        .request(server)
+        .post('/api/login')
+        .type('form')
+        .send(login)
+        .end(function(err, res) {
+          res.should.have.status(400);
+          done();
+        });
+    });
+
+    it('should have valid data input in password', function(done) {
+      var login = {
+        username: 'rmcrary',
+        password: '*&$('
+      };
+      chai
+        .request(server)
+        .post('/api/login')
+        .type('form')
+        .send(login)
+        .end(function(err, res) {
+          res.should.have.status(400);
+          done();
+        });
+    });
+
+    it('should have valid data input in both fields', function(done) {
+      var login = {
+        'username': 'rmcrary',
+        'password': 'rI73l'
+      };
+      chai
+        .request(server)
+        .post('/api/login')
+        .type('form')
+        .send(login)
+        .end(function(err, res) {
+            console.log(res.body)
+          res.should.have.status(200);
+          done();
+        });
+    });
   });
 
-  it("to append period to leading titles", function() {
-    expect(login("dr zhivago")).to.equal("Dr. Zhivago");
-  });
+  //   Add new expense line testing
+  //   describe('expenses route', function() {
+  //     it('', function(done) {
+  //       var login = {
 
-  it("to not change properly cased strings", function() {
-    expect(login("Mr. Alexander Pushkin")).to.equal("Mr. Alexander Pushkin");
-  });
+  //       };
 
-  it("to add periods to properly capitalized titles", function() {
-    expect(login("Mr Alexander Pushkin")).to.equal("Mr. Alexander Pushkin");
-  });
-
-  it("to properly case mixed case strings", function() {
-    expect(login("Mr AlEXANDER PushKIn")).to.equal("Mr. Alexander Pushkin");
-  });
+  //       chai
+  //         .request(server)
+  //         .post('/api/expenses')
+  //         .send(login)
+  //         .end(function(err, res) {
+  //           res.should.have.status(400);
+  //           done();
+  //         });
+  //     });
+  //   });
 });
