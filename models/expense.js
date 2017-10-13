e473b42ccf57e90f4cdb0cf1e7611f8c14b1d659
Rich McRary <rgmcrary@gmail.com>
2017-10-13 14:52:03 -0400

    Added association codes

--- models/expense.js
+++ models/expense.js
@@ -1,48 +1,48 @@
+var Employee = require('./employee');
+
 module.exports = function(sequelize, DataTypes) {
+  var Expense = sequelize.define('Expense', {
+    expName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
 
-   var Expense = sequelize.define("Expense", {
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
 
-      expName: {
-        type: DataTypes.STRING,
-        allowNull: false
-     },
-      
-      date: {
-         type: DataTypes.DATE,
-         allowNull: false
-      },
+    duration: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
 
-      duration: {
-         type: DataTypes.INTEGER,
-         allowNull: false
-      },
+    costType: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
 
-      costType: {
-         type: DataTypes.STRING,
-         allowNull: false
-      },
+    amount: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
 
-      amount: {
-         type: DataTypes.DECIMAL,
-         allowNull: false
-      },
-
-      approved: {
-         type: DataTypes.BOOLEAN,
-         allowNull: false,
-         defaultValue: false
-      }
+    approved: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
+    
   });
 
-  Expense.associate = function(models) {
-    // We're saying that a Post should belong to an Author
-    // A Post can't be created without an Author due to the foreign key constraint
+   Expense.associate = function(models) {
+  //   // We're saying that a Post should belong to an Author
+  //   // A Post can't be created without an Author due to the foreign key constraint
     Expense.belongsTo(models.Employee, {
-      foreignKey: {
-        allowNull: false
-      }
+      //foreignKey: 'EmployeeId',
+      targetKey: 'id'
     });
-  };
+   };
 
   return Expense;
 };
