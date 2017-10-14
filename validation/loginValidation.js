be7db5f6b87ef1991f836fc09b1e5c29b137ce22
Rich McRary <rgmcrary@gmail.com>
2017-10-14 17:43:01 -0400

    renamed file

--- validation/loginValidation.js
+++ validation/loginValidation.js
@@ -2,7 +2,7 @@ var Joi = require('joi');
 
 module.exports = {
  body: {
-   username: Joi.string().regex(/[a-zA-Z0-9]{3,30}/).required(),
+   userId: Joi.string().regex(/[a-zA-Z0-9]{3,30}/).required(),
    password: Joi.string().regex(/[a-zA-Z0-9]{3,30}/).required()
  }
 };
