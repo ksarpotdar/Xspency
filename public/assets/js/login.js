63388f9b2b51a724b8a71c35e322490390e0ec20
Rich McRary <rgmcrary@gmail.com>
2017-10-14 17:43:35 -0400

    client side login logic

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,19 +1,30 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
+  var mgrRedirect;
+
   $('#login').on('click', function(event) {
     var newLogin = {
-      userId: $('#username').val,
-      password: $('#password').val,
-      mgrFlag: false
+      userId: $('#username').val(),
+      password: $('#password').val(),
+      managerView: $('#manager').is(':checked')
     };
-
+    mgrRedirect = $('#manager').is(':checked');
     // Send the POST request
     $.ajax('/api/login', {
       type: 'POST',
       data: newLogin
     }).then(function(result) {
-      if (result.status === 401) {
+      if (result.response === 'Invalid username or password') {
         alert('Incorrect username or password.');
+      } else {
+        $.cookie('authenticated', true);
+        if (mgrRedirect === true) {
+          var ids = result.empIds.toString();
+          window.location.href = '/manager/' + ids;
+        } else {
+          var id = result.id;
+          window.location.href = '/employee/' + id;
+        }
       }
     });
   });
