d2cd3436a9cdfdf2c564923bb860c7f1b07b107a
Rich McRary <rgmcrary@gmail.com>
2017-10-14 20:12:42 -0400

    created add and delete functionality

--- public/assets/js/empexpenses.js
+++ public/assets/js/empexpenses.js
@@ -1,33 +1,38 @@
-var Controller = require('../controllers');
+if (!$.cookie('authenticated')) {
+  window.location.href = '/';
+}
+$(function() {
+  $('#addButton').on('click', function(event) {
+    var newExpense = {
+      expName: $('#expName').val(),
+      date: $('#date').val(),
+      duration: $('#duration').val(),
+      costType: $('#costType').val(),
+      amount: $('#amount').val(),
+      EmployeeId: $('#employeeId').val()
+    };
 
-var newLogin = {
-  userId: $('#username').val,
-  password: $('#password').val,
-  mgrFlag: false
-};
+    // Create a new expense
+    $.ajax('/api/expense', {
+      type: 'POST',
+      data: newExpense
+    }).then(function() {
+      console.log('created new expense line');
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
 
-// Create a new expense
-$.ajax('/api/expense', {
-  type: 'POST',
-  data: newLogin
-}).then(function() {
-  on('click');
-  console.log('created new expense line');
-  // Reload the page to get the updated list
-  location.reload();
-});
-
-// Delete an expense
-$.ajax('/api/expense/delete/:id' + id, {
-  type: 'PUT'
-}).then(function() {
-  console.log('changed to devoured');
-  // Reload the page to get the updated list
-  location.reload();
-});
-
-$('.create-form').on('submit', function(event) {
-  // Make sure to preventDefault on a submit event.
-  event.preventDefault();
-
+  // Delete an expense
+  $('.deleteButton').on('click', function(event) {
+    var id = $(this).data('id');
+    // Send the PUT request.
+    $.ajax('/api/expense/delete/' + id, {
+      type: 'PUT'
+    }).then(function() {
+      console.log('deleted expense item');
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
 });
