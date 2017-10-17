da31df60c7392ef8d4bbdb79d15bd8221621dc06
SobeDog007 <sdsober@gmail.com>
2017-10-17 17:32:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var db = require('./models');
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 var app = express();
 
